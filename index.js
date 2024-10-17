da081ac316949ae13a2f489c24813820e7167971
raymonddong71 <raymonddong71@gmail.com>
2024-10-17 14:47:36 -0400

    Make the card work
    make it so that there is a card after u submit

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ const DOMSelectors = {
   container: document.querySelector(".container"),
 };
 
-const liItem = DOMSelectors.items;
-const item = Array.form(liItem);
-item.forEach((el) => (el.style.color = "red"));
+// const liItem = DOMSelectors.items;
+// const item = Array.form(liItem);
+// item.forEach((el) => (el.style.color = "red"));
 
-//DOMSelectors.button.addEventListener("click", function (event) {
-//console.log(event.target.parentElement);
-//event.target.parentElement.style.backgroundColor = "red";
-//});
+// //DOMSelectors.button.addEventListener("click", function (event) {
+// //console.log(event.target.parentElement);
+// //event.target.parentElement.style.backgroundColor = "red";
+// //});
 
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  DOMSelectors.button.style.backgroundColor = "white";
-  setTimeout(function () {
-    DOMSelectors.button.style.backgroundColor = "lightgrey";
-  }, 50);
-  const iValue = document.querySelector("#input");
-  DOMSelectors.container.insertAdjacentHTML(
-    "beforeheand",
-    `<div class ="card"><h2 class="card-header">${iValue.value}</h2></div>`
-  );
-});
+// DOMSelectors.form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   DOMSelectors.button.style.backgroundColor = "white";
+//   setTimeout(function () {
+//     DOMSelectors.button.style.backgroundColor = "lightgrey";
+//   }, 50);
+//   const iValue = document.querySelector("#input");
+
+//   DOMSelectors.container.insertAdjacentHTML(
+//     "beforeheand",
+//     `<div class ="card"><h2 class="card-header">${iValue.value}</h2></div>`
+//   );
+// });
