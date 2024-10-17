3810b8615c1cd3f7855014e611cffc095ccfcdf0
raymondd71 <raymondd71@nycstudents.net>
2024-10-17 12:28:45 -0400

    ewqeqw
    qweqweq

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
-  text: document.querySelector("#text"),
-  box: document.getElementById("big black box"),
-  points: document.querySelectorAll(".point"), //node list
+  header: document.querySelector("h1"),
+  button: document.querySelectorAll(".btn"),
+  items: document.querySelector("li"),
+  cardHeader: document.querySelector(".card-header"),
+  form: document.querySelector(".form"),
+  container: document.querySelector(".container"),
 };
 
-function backgroundAndText(background, text) {
-  background.style.backroundColor = "red";
-  text.textContent = "This is now a bigger red box";
-  text.styke.fontSize = "40px";
-}
+const liItem = DOMSelectors.items;
+const item = Array.form(liItem);
+item.forEach((el) => (el.style.color = "red"));
 
-DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+//DOMSelectors.button.addEventListener("click", function (event) {
+//console.log(event.target.parentElement);
+//event.target.parentElement.style.backgroundColor = "red";
+//});
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  DOMSelectors.button.style.backgroundColor = "white";
+  setTimeout(function () {
+    DOMSelectors.button.style.backgroundColor = "lightgrey";
+  }, 50);
+  const iValue = document.querySelector("#input");
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeheand",
+    `<div class ="card"><h2 class="card-header">${iValue.value}</h2></div>`
+  );
 });
